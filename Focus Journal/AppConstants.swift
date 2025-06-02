c6d8b6b445beab28e3d6f15863797046735ec488
Ben Gohlke <jcgohlke@gmail.com>
2025-06-02 11:43:35 -0400

    Squashed commit of the following:
    commit b5a2546c425b54556128c3a239654bfaa977822c
    Author: Ben Gohlke <jcgohlke@gmail.com>
    Date:   Mon Jun 2 11:42:27 2025 -0400
    
        Added app constants file

---
+++ Focus Journal/AppConstants.swift
@@ -0,0 +1,15 @@
+//
+//  AppConstants.swift
+//  Focus Journal
+//
+//  Created by Ben Gohlke on 5/27/25.
+//  Copyright © 2025 Adapty. All rights reserved.
+//
+
+struct AppConstants {
+  struct Adapty {
+    static let apiKey = "API key goes here"
+    static let accessLevelID = "premium"
+    static let placementID = "on_show_history"
+  }
+}
