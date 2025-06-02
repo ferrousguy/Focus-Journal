7c20e650469279e4a4c366515b7c15e92d4cecbd
Ben Gohlke <jcgohlke@gmail.com>
2025-06-02 11:33:17 -0400

    Initial Commit

---
+++ Focus Journal/Focus_JournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Focus_JournalApp.swift
+//  Focus Journal
+//
+//  Created by Joben Gohlke on 6/2/25.
+//
+
+import SwiftUI
+
+@main
+struct Focus_JournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
